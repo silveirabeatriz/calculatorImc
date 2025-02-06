9b60349f7765eca21cab1cc42deaa3d1867c7738
Beatriz Silveira <beatrizsvra@gmail.com>
2025-02-06 12:47:59 +0100

    calculate

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,33 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes EditText
+        // Criar var e associar o componente
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        //Recuperar o button
+        //Colocar acao no button e recuperar o texto
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+        btnCalculate.setOnClickListener{
+
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+
+            
+            println("Acao do botao " + peso + altura)
+        }
+
+
     }
 }
