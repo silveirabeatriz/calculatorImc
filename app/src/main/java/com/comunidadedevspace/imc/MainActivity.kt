975c91d4c4e04d75c780faa0985714b5db427730
Beatriz Silveira <beatrizsvra@gmail.com>
2025-02-10 12:13:46 +0100

    message

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -22,12 +23,30 @@ class MainActivity : AppCompatActivity() {
 
         btnCalculate.setOnClickListener{
 
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
-            println("O seu IMC é: " + resultado)
+            val pesoStr: String= edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == ""){
+                //Mostrar msg para o usuário Snackbar
+
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Preeencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                println("O seu IMC é: " + resultado)
+            }
         }
 
 
